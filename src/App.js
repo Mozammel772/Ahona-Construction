51d076c3875098538f2de2d6e32f31bba6a9cbdd
mozammelhosene <mozammelhosenjabir@gmail.comm>
2023-02-02 16:55:48 +0600

    more page create now

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import About from "./Pages/About/About";
+import AhonaAgroFarm from "./Pages/AhonaAgroFarm/AhonaAgroFarm";
+import AhonaConstruction from "./Pages/AhonaConstruction/AhonaConstruction";
+import AhonaEnterprize from "./Pages/AhonaEnterprize/AhonaEnterprize";
+import ALMBricks from "./Pages/ALMBricks/ALMBricks";
 import Contact from "./Pages/Contact/Contact";
 import Footer from "./Pages/Footer/Footer";
 import Home from "./Pages/Home/Home";
 import Header from "./Pages/Navbar/Header";
 import NotFound from "./Pages/NotFound/NotFound";
 import Company from "./Pages/OurCompany/Company";
+import SRAgroFarm from "./Pages/SRAgroFarm/SRAgroFarm";
+import SRNoorInternational from "./Pages/SRNoorInternational/SRNoorInternational";
+
 
 function App() {
   return (
@@ -14,12 +21,18 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />}></Route>
-        <Route path="/about" element={<About/>}></Route>
-        <Route path="/contact" element={<Contact/>}></Route>
-        <Route path="/our-company" element={<Company/>}></Route>
-        <Route path="*" element={<NotFound/>}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
+        <Route path="/our-company" element={<Company />}></Route>
+        <Route path="/ahona-construction" element={<AhonaConstruction/>}></Route>
+        <Route path="/ahona-enterprize" element={<AhonaEnterprize/>}></Route>
+        <Route path="/ahona-agro-farm" element={<AhonaAgroFarm/>}></Route>
+        <Route path="/ahona-alm-bricks" element={<ALMBricks/>}></Route>
+        <Route path="/ahona-sr-agro-farm" element={<SRAgroFarm/>}></Route>
+        <Route path="/ahona-sr-noor-international" element={<SRNoorInternational/>}></Route>
+        <Route path="*" element={<NotFound />}></Route>
       </Routes>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
---
+++ src/Pages/AhonaConstruction/AhonaConstruction.js
@@ -0,0 +1,85 @@
+import React from "react";
+import ahona1 from "../../asset/galary1.jpg";
+import ahona from "../../asset/galary2.jpg";
+import "./AhonaConstruction.css";
+
+const AhonaConstruction = () => {
+  return (
+    <div>
+      <h1 className="text-5xl font-bold my-10 text-center">
+        Welcome to <span className="text-[#ff600e]">Ahona</span> Construction
+      </h1>
+      <div id="ahonaConstruction" className="hero min-h-screen ">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <img
+            src={ahona}
+            className="w-full lg:h-[300px] rounded-lg shadow-2xl "
+            alt="Avator"
+          />
+          <div>
+            <h1 className="text-3xl font-bold">
+              1. Build <span className="text-[#ff600e]">Construction</span> !
+            </h1>
+            <p className="py-6 w-full font-medium text-[#727272]">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque
+              asperiores amet voluptas veritatis, pariatur libero saepe suscipit
+              assumenda animi totam architecto laborum! Repudiandae eligendi
+              earum nulla deserunt autem, totam hic fugit tenetur? Expedita, quo
+              perferendis hic, alias voluptatum quae voluptatibus deserunt
+              magnam dolore nam itaque necessitatibus assumenda quam a.
+            </p>
+            <button id="btn">Contact Us</button>
+          </div>
+        </div>
+      </div>
+      <div id="ahonaConstruction" className="hero min-h-screen my-10 ">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <img
+            src={ahona1}
+            className="w-full lg:h-[300px] rounded-lg shadow-2xl  "
+            alt="Avator"
+          />
+          <div>
+            <h1 className="text-3xl font-bold">
+              1. Road <span className="text-[#ff600e]">Construction</span> !
+            </h1>
+            <p className="py-6 w-full font-medium text-[#727272]">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque
+              asperiores amet voluptas veritatis, pariatur libero saepe suscipit
+              assumenda animi totam architecto laborum! Repudiandae eligendi
+              earum nulla deserunt autem, totam hic fugit tenetur? Expedita, quo
+              perferendis hic, alias voluptatum quae voluptatibus deserunt
+              magnam dolore nam itaque necessitatibus assumenda quam a.
+            </p>
+            <button id="btn">Contact Us</button>
+          </div>
+        </div>
+      </div>
+      <div id="ahonaConstruction" className="hero min-h-screen ">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <img
+            src={ahona}
+            className="w-full lg:h-[300px] rounded-lg shadow-2xl "
+            alt="Avator"
+          />
+          <div>
+            <h1 className="text-3xl font-bold">
+              1. Build <span className="text-[#ff600e]">Construction</span> !
+            </h1>
+            <p className="py-6 w-full font-medium text-[#727272]">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque
+              asperiores amet voluptas veritatis, pariatur libero saepe suscipit
+              assumenda animi totam architecto laborum! Repudiandae eligendi
+              earum nulla deserunt autem, totam hic fugit tenetur? Expedita, quo
+              perferendis hic, alias voluptatum quae voluptatibus deserunt
+              magnam dolore nam itaque necessitatibus assumenda quam a.
+            </p>
+            <button id="btn">Contact Us</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AhonaConstruction;
