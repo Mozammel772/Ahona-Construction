b03fca272c05b31a4c9d5d5d3fb22e34c1967bd5
Mozammel Hosen <mozammelhosenjabir@gmail.comgit config --global user.name Mozammel>
2023-03-30 07:15:05 +0600

    ll okay and build now

--- src/Pages/Home/Carosule/Carosule.js
+++ src/Pages/Home/Carosule/Carosule.js
@@ -1,84 +1,40 @@
 import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick-theme.css";
+import "slick-carousel/slick/slick.css";
 import Carosule4 from "../../../asset/bg2.jpg";
 import Carosule5 from "../../../asset/bg3.jpeg";
-import Carosule2 from "../../../asset/carosule2.jpg";
-import Carosule3 from "../../../asset/carosule3.jpg";
-import "./Carosule.css";
+// import "./Carosule.css";
+
+var settings = {
+  dots: true,
+  infinite: true,
+  autoplaySpeed: 3000,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+};
 
 const Carosule = () => {
   return (
-    <div>
-      <div class="carousel w-full">
-        <div id="slide2" class="carousel-item relative w-full">
+    <>
+      <Slider {...settings}>
+        <div>
           <img
+            className="h-[400px] w-full"
             src={Carosule4}
-            class="w-full h-[500px] rounded image-container"
-            alt="Avator"
+            alt="Not Avaiable Image Silder"
           />
-          <div class="absolute flex justify-between items-center transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <div>
-              <a href="#slide2" class="btn btn-circle">
-                ❮
-              </a>
-            </div>
-
-            <div>
-              <a href="#slide2" class="btn btn-circle">
-                ❯
-              </a>
-            </div>
-          </div>
         </div>
-        <div id="slide3" class="carousel-item relative w-full">
-          <img src={Carosule5} class="w-full h-[500px] rounded" alt="Avator" />
-          <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2 items-center">
-            <div>
-              <a href="#slide3" class="btn btn-circle">
-                ❮
-              </a>
-            </div>
-
-            <div>
-              <a href="#slide3" class="btn btn-circle">
-                ❯
-              </a>
-            </div>
-          </div>
+        <div>
+          <img
+            className="h-[400px] w-full"
+            src={Carosule5}
+            alt="Not Avaiable Image Silder"
+          />
         </div>
-        <div id="slide4" class="carousel-item relative w-full">
-          <img src={Carosule2} class="w-full h-[500px] rounded" alt="Avator" />
-          <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2 items-center">
-            <div>
-              <a href="#slide4" class="btn btn-circle">
-                ❮
-              </a>
-            </div>
-
-            <div>
-              <a href="#slide4" class="btn btn-circle">
-                ❯
-              </a>
-            </div>
-          </div>
-        </div>
-        <div id="slide5" class="carousel-item relative w-full">
-          <img src={Carosule3} class="w-full h-[500px] rounded" alt="Avator" />
-          <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2 items-center">
-            <div>
-              <a href="#slide5" class="btn btn-circle">
-                ❮
-              </a>
-            </div>
-
-            <div>
-              <a href="#slide5" class="btn btn-circle">
-                ❯
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+      </Slider>
+    </>
   );
 };
 
--- src/Pages/Home/Construction/Construction.js
+++ src/Pages/Home/Construction/Construction.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import allcons from "../../../asset/allcons.jpg";
 import balu from "../../../asset/balu1.jpg";
 import cement from "../../../asset/cement.jpg";
@@ -18,97 +19,97 @@ const Construction = () => {
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5  py-10 px-5" id="construction">
         <div className="card  bg-white shadow-xl">
           <figure className="px-2 pt-2">
-            <img src={bricks} alt="Shoes" className="h-[200px] w-[200px] rounded-xl" />
+            <img src={bricks} alt="Shoes" className="h-[200px] w-[300px] rounded-xl" />
           </figure>
           <div className="card-body ">
             <h2 className="card-title text-2xl">1. Bricks!</h2>
             <p className="text-[#727272]">Our Best Worker and make by House</p>
             <div className="card-actions">
-              <button className="btnn">Buy Now</button>
+               <Link className="btnn" to="/contact">Buy Now</Link>
             </div>
           </div>
         </div>
         <div className="card  bg-white shadow-xl">
           <figure className="px-2 pt-2">
-            <img src={balu} alt="Shoes" className="h-[200px] w-[200px] rounded-xl" />
+            <img src={balu} alt="Shoes" className="h-[200px] w-[300px] rounded-xl" />
           </figure>
           <div className="card-body">
             <h2 className="card-title text-2xl">2. Balu!</h2>
             <p>If a dog chews shoes whose shoes does he choose?</p>
             <div className="card-actions">
-              <button className="btnn">Buy Now</button>
+               <Link className="btnn" to="/contact">Buy Now</Link>
             </div>
           </div>
         </div>
         <div className="card  bg-white shadow-xl">
           <figure className="px-2 pt-2">
-            <img src={cement} alt="Shoes" className="h-[200px] w-[200px] rounded-xl" />
+            <img src={cement} alt="Shoes" className="h-[200px] w-[300px] rounded-xl" />
           </figure>
           <div className="card-body ">
             <h2 className="card-title text-2xl">3. Cement!</h2>
             <p>If a dog chews shoes whose shoes does he choose?</p>
             <div className="card-actions">
-              <button className="btnn">Buy Now</button>
+               <Link className="btnn" to="/contact">Buy Now</Link>
             </div>
           </div>
         </div>
         <div className="card  bg-white shadow-xl">
           <figure className="px-2 pt-2">
-            <img src={ispat} alt="Shoes" className="h-[200px] w-[200px] rounded-xl" />
+            <img src={ispat} alt="Shoes" className="h-[200px] w-[300px] rounded-xl" />
           </figure>
           <div className="card-body ">
             <h2 className="card-title text-2xl">4. Ispat!</h2>
             <p>If a dog chews shoes whose shoes does he choose?</p>
             <div className="card-actions">
-              <button className="btnn">Buy Now</button>
+               <Link className="btnn" to="/contact">Buy Now</Link>
             </div>
           </div>
         </div>
         <div className="card  bg-white shadow-xl">
           <figure className="px-2 pt-2">
-            <img src={sectionbalu} alt="Shoes" className="h-[200px] w-[200px] rounded-xl" />
+            <img src={sectionbalu} alt="Shoes" className="h-[200px] w-[300px] rounded-xl" />
           </figure>
           <div className="card-body ">
             <h2 className="card-title text-2xl">5. Sectionbalu!</h2>
             <p>If a dog chews shoes whose shoes does he choose?</p>
             <div className="card-actions">
-              <button className="btnn">Buy Now</button>
+               <Link className="btnn" to="/contact">Buy Now</Link>
             </div>
           </div>
         </div>
         <div className="card  bg-white shadow-xl">
           <figure className="px-2 pt-2">
-            <img src={allcons} alt="Shoes" className="h-[200px] w-[200px] rounded-xl" />
+            <img src={allcons} alt="Shoes" className="h-[200px] w-[300px] rounded-xl" />
           </figure>
           <div className="card-body ">
             <h2 className="card-title text-2xl">6. allcons!</h2>
             <p>If a dog chews shoes whose shoes does he choose?</p>
             <div className="card-actions">
-              <button className="btnn">Buy Now</button>
+               <Link className="btnn" to="/contact">Buy Now</Link>
             </div>
           </div>
         </div>
         <div className="card  bg-white shadow-xl">
           <figure className="px-2 pt-2">
-            <img src={banner1} alt="Shoes" className="h-[200px] w-[200px] rounded-xl" />
+            <img src={banner1} alt="Shoes" className="h-[200px] w-[300px] rounded-xl" />
           </figure>
           <div className="card-body ">
             <h2 className="card-title text-2xl">7. WorkShrit!</h2>
             <p>If a dog chews shoes whose shoes does he choose?</p>
             <div className="card-actions">
-              <button className="btnn">Buy Now</button>
+               <Link className="btnn" to="/contact">Buy Now</Link>
             </div>
           </div>
         </div>
         <div className="card  bg-white shadow-xl">
           <figure className="px-2 pt-2">
-            <img src={banner2} alt="Shoes" className="h-[200px] w-[200px] rounded-xl" />
+            <img src={banner2} alt="Shoes" className="h-[200px] w-[300px] rounded-xl" />
           </figure>
           <div className="card-body ">
             <h2 className="card-title text-2xl">8. Work!</h2>
             <p>If a dog chews shoes whose shoes does he choose?</p>
             <div className="card-actions">
-              <button className="btnn">Buy Now</button>
+               <Link className="btnn" to="/contact">Buy Now</Link>
             </div>
           </div>
         </div>
